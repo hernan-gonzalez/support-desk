8c1968ff9b13a40e690b35558a20cf4bb60eb058
Hernan Gonzalez <hegonzalez@transperfect.com>
2022-08-19 09:33:27 -0400

    Added userRoutes, userController, ErrorHandler

---
+++ backend/routes/usesRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router = express.Router()
+const { registerUser, loginUser } = require('../controllers/userController')
+
+router.post('/', (req, res) => {
+    registerUser(req, res)
+})
+
+router.post('/login', (req, res) => {
+    loginUser(req,res)
+})
+
+module.exports = router
