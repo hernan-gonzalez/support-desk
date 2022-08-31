69e9778075e287c0cc46526dd7f4a62649a01c0e
Hernan Gonzalez <hegonzalez@transperfect.com>
2022-08-31 16:43:02 -0400

    Added Create ticket functionality

--- frontend/src/pages/NewTicket.jsx
+++ frontend/src/pages/NewTicket.jsx
@@ -1,18 +1,48 @@
-import { useState } from "react"
-import { useSelector } from "react-redux"
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { createTicket, reset } from "../features/tickets/ticketSlice";
+import Spinner from "../components/Spinner";
+import BackButton from "../components/BackButton";
 
 function NewTicket() {
-    const { user } = useSelector((state) => state.auth)
-    const [name] = useState(user.name)
-    const [email] = useState(user.email)
-    const [product, setProduct] = useState('iPhone')
-    const [description, setDescription] = useState('')
+    const { user } = useSelector((state) => state.auth);
+    const { isLoading, isError, isSuccess, message } = useSelector(
+        (state) => state.ticket
+    );
+    const [name] = useState(user.name);
+    const [email] = useState(user.email);
+    const [product, setProduct] = useState("iPhone");
+    const [description, setDescription] = useState("");
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (isError) {
+            toast.error(message);
+        }
+        if (isSuccess) {
+            dispatch(reset());
+            navigate("/tickets");
+        }
+        dispatch(reset());
+    }, [dispatch, isError, isSuccess, navigate, message]);
 
     const onSubmit = (e) => {
-        e.preventDefault()
+        e.preventDefault();
+
+        dispatch(createTicket({ product, description }));
+    };
+
+    if (isLoading) {
+        return <Spinner />;
     }
+
     return (
         <>
+            <BackButton url="/" />
             <section className="heading">
                 <h1>Create New Ticket</h1>
                 <p>Please fill out the form below</p>
@@ -20,17 +50,32 @@ function NewTicket() {
 
             <section className="form">
                 <div className="form-group">
-                    <label htmlFor="name" >Customer Name</label>
-                    <input type="text" className="form-control" value={name} disabled />
+                    <label htmlFor="name">Customer Name</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        value={name}
+                        disabled
+                    />
                 </div>
                 <div className="form-group">
-                    <label htmlFor="email" >Customer Email</label>
-                    <input type="text" className="form-control" value={email} disabled />
+                    <label htmlFor="email">Customer Email</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        value={email}
+                        disabled
+                    />
                 </div>
                 <form onSubmit={onSubmit}>
                     <div className="form-group">
                         <label htmlFor="product">Product</label>
-                        <select name="product" id="product" value={product} onChange={(e) => setProduct(e.target.value)}>
+                        <select
+                            name="product"
+                            id="product"
+                            value={product}
+                            onChange={(e) => setProduct(e.target.value)}
+                        >
                             <option value="iPhone">iPhone</option>
                             <option value="Macbook Pro">Macbook Pro</option>
                             <option value="iMac">iMac</option>
@@ -38,8 +83,17 @@ function NewTicket() {
                         </select>
                     </div>
                     <div className="form-group">
-                        <label htmlFor="description" >Description of the issue</label>
-                        <textarea name="description" id="description" className="form-control" placeholder="Description" value={description} onChange={(e) => setDescription(e.target.value)}></textarea>
+                        <label htmlFor="description">
+                            Description of the issue
+                        </label>
+                        <textarea
+                            name="description"
+                            id="description"
+                            className="form-control"
+                            placeholder="Description"
+                            value={description}
+                            onChange={(e) => setDescription(e.target.value)}
+                        ></textarea>
                     </div>
                     <div className="form-group">
                         <button className="btn btn-block">Submit</button>
@@ -47,7 +101,7 @@ function NewTicket() {
                 </form>
             </section>
         </>
-    )
+    );
 }
 
-export default NewTicket
+export default NewTicket;
